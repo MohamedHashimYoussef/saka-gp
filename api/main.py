cad0a3c577e15739e3ec7611db945acb99b6d9d8
Mohamed Hashim Youssef <57812918+MohamedHashimYoussef@users.noreply.github.com>
2020-04-17 01:39:29 +0200

    last

--- api/main.py
+++ api/main.py
@@ -1,23 +1,33 @@
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
 def Prediction(age , HeartRate , Gender , bloodpressure , Diabetic , Smooker , Drinker , Athlete , BMI):
     data = pd.read_excel('api/data.xlsx' , sheet_name="datasheet")
-    data['AGE'] = pd.cut(data['AGE'] , bins=[0 , 50 , 200] , labels=["young" , "old"])
-    data['Heart Rate'] = pd.cut(data['Heart Rate'] , bins=[0 , 60 , 101 , 400] , labels=["bad" , "good" , "Bad"])
-    data['AGE'] = pd.factorize( data['AGE'] )[0]
-    data['Heart Rate'] = pd.factorize(data['Heart Rate'])[0]
+    #data['AGE'] = pd.cut(data['AGE'] , bins=[0 , 50 , 200] , labels=["young" , "old"])
+    #data['Heart Rate'] = pd.cut(data['Heart Rate'] , bins=[0 , 60 , 101 , 400] , labels=["bad" , "good" , "Bad"])
+    #data['AGE'] = pd.factorize( data['AGE'] )[0]
+    #data['Heart Rate'] = pd.factorize(data['Heart Rate'])[0]
+    #print(data)
     data = data.to_numpy()
     X = data[: , :9]
     Y = data[: , 9]
 
     X_train , X_test , Y_train , Y_test = train_test_split(X , Y , test_size=0.2 , random_state=0)
+    gnb = DecisionTreeClassifier(random_state=0, max_depth=13)
 
-    gnb = DecisionTreeClassifier()
-    gnb.fit(X_train , Y_train)
+    gnb.fit(X_train, Y_train)
+    estimator = 200
+    btd = AdaBoostClassifier(gnb,
+                             algorithm="SAMME",
+                             n_estimators=estimator, random_state=0
+                             )
+    btd.fit(X_train, Y_train)
+    #res = btd.predict(X_test)
 # Predict all data and calculate accuracy
 #Y_pred = gnb.predict(X_test)
 #cnt = 0
@@ -37,5 +47,5 @@ def Prediction(age , HeartRate , Gender , bloodpressure , Diabetic , Smooker , D
 #Athlete = int(input("Enter Athlete : "))
 #BMI = int(input("Enter BMI : "))
     d = np.array([[age , HeartRate , Gender , bloodpressure , Diabetic , Smooker , Drinker , Athlete , BMI]])
-    prediction = gnb.predict(d)
+    prediction = btd.predict(d)
     return prediction
